c945dcfa4b88aee1fbb4d8db62db1bdd60f0d733
Raffaele Sena <raff367@gmail.com>
2023-07-17 23:36:07 -0700

    Some utility methods to deal with image tiles

---
+++ util/tiles.go
@@ -0,0 +1,70 @@
+package util
+
+import (
+	"errors"
+	"image"
+	"image/png"
+	"io"
+
+	"github.com/hajimehoshi/ebiten/v2"
+)
+
+// struct Tiles contains a list of image tiles.
+// It also contains the tile dimension
+type Tiles struct {
+	Width   int             // tile width
+	Height  int             // tile height
+	Columns int             // number of tiles per row
+	Rows    int             // number of rows
+	List    []*ebiten.Image // list of tiles (0..Width*Height)
+}
+
+// Item return the tile at (linear) position i
+func (t *Tiles) Item(i int) *ebiten.Image {
+	if i < 0 || i >= len(t.List) {
+		return nil
+	}
+
+	return t.List[i]
+}
+
+// At return the tile at the coordinates x, y
+func (t *Tiles) At(x, y int) *ebiten.Image {
+	if x < 0 || x >= t.Columns || y < 0 || y >= t.Rows {
+		return nil
+	}
+
+	return t.List[x+y*t.Width]
+}
+
+// ReadTiles read the tiles from a png file
+func ReadTiles(r io.Reader, nx, ny int) (*Tiles, error) {
+	img, err := png.Decode(r)
+	if err != nil {
+		return nil, err
+	}
+
+	iw, ih := img.Bounds().Dx(), img.Bounds().Dy()
+	if iw%nx != 0 || ih%ny != 0 {
+		return nil, errors.New("invalid cells image dimension")
+	}
+
+	tw, th := iw/nx, ih/ny
+
+	ebimg := ebiten.NewImageFromImage(img)
+	p := image.Rect(0, 0, tw, th)
+
+	var tiles = Tiles{Width: tw, Height: th, Rows: ny, Columns: nx}
+
+	for v := 0; v < ny; v++ {
+		for h := 0; h < nx; h++ {
+			tile := ebimg.SubImage(p).(*ebiten.Image)
+			tiles.List = append(tiles.List, tile)
+			p = p.Add(image.Pt(tw, 0))
+		}
+
+		p.Add(image.Pt(-iw, th))
+	}
+
+	return &tiles, nil
+}
