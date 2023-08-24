959d858d04e6f9a37f45038ea22bb0edfc4ed91f
Raffaele Sena <raff367@gmail.com>
2023-08-23 23:04:19 -0700

    Shuffle cards (temporary disabled to help debug an issue) and added method to find the match between two cards.

--- spot/cards.go
+++ spot/cards.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+//"fmt"
+//"math/rand"
+)
+
 const (
 	ndups = 8  // 8 copies of each object (and number of objects per card)
 	nsyms = 57 // total number of objects (and number of cards)
@@ -27,18 +32,55 @@ func getCards() (cards [][]int) {
 	}
 
 	for k := 0; k < ndups; k++ {
-		card := []int{getsym()}
+		first := getsym()
 
 		for r := 0; r < ndups; r++ {
+			card := []int{first}
+
 			for k := 1; k < ndups; k++ {
 				card = append(card, getsym())
 			}
 
-			fmt.Println(card)
+			//rand.Shuffle(len(card), func(i, j int) {
+			//	card[i], card[j] = card[j], card[i]
+			//})
+
 			cards = append(cards, card)
-			card = []int{card[0]}
 		}
 	}
 
+	//rand.Shuffle(len(cards), func(i, j int) {
+	//	cards[i], cards[j] = cards[j], cards[i]
+	//})
+
 	return
 }
+
+func match(c1, c2 []int) int {
+	for _, v1 := range c1 {
+		for _, v2 := range c2 {
+			if v1 == v2 {
+				return v1
+			}
+		}
+	}
+
+	return 0
+}
+
+/*
+func main() {
+	cards := getCards()
+
+	for _, c := range cards {
+		fmt.Printf("%2v\n", c)
+	}
+
+	fmt.Println()
+
+	for i := 1; i < len(cards); i++ {
+		c1, c2 := cards[i-1], cards[i]
+		fmt.Printf("%2v %2v -> %v\n", c1, c2, match(c1, c2))
+	}
+}
+*/
