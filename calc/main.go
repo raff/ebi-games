4766019c9c45bf82e822c0936cf76deac107b3e3
Raffaele Sena <raff367@gmail.com>
2023-09-19 21:41:07 -0700

    Some logic for the calculator (but it's incorrect because right now it works like RPN)

--- calc/main.go
+++ calc/main.go
@@ -7,6 +7,8 @@ import (
 	"image/png"
 	"io"
 	"log"
+	"strconv"
+	"strings"
 
 	_ "embed"
 
@@ -66,6 +68,9 @@ var displayCoords = coords(13, 33, 77, 9) // display
 type Game struct {
 	redraw bool
 	sel    ebiten.Key
+
+	acc float64
+	val string
 
 	ima *ebiten.Image
 	inv *ebiten.Image
@@ -151,8 +156,12 @@ func (g *Game) Update() error {
 
 	for k, v := range buttons {
 		if ebiten.IsKeyPressed(k) || (ebiten.IsMouseButtonPressed(ebiten.MouseButtonLeft) && p.In(v)) {
-			g.sel = k
-			g.redraw = true
+			if g.sel != k {
+				g.sel = k
+				g.redraw = true
+
+				g.processKey(k)
+			}
 			return nil
 		}
 	}
@@ -163,6 +172,58 @@ func (g *Game) Update() error {
 	}
 
 	return nil
+}
+
+func (g *Game) processKey(k ebiten.Key) {
+	switch k {
+	case ebiten.Key0:
+		if g.val != "" {
+			g.val += "0"
+		}
+
+	case ebiten.Key1, ebiten.Key2, ebiten.Key3, ebiten.Key4, ebiten.Key5, ebiten.Key6, ebiten.Key7, ebiten.Key8, ebiten.Key9:
+		g.val += string(k - ebiten.Key0 + '0')
+
+	case ebiten.KeyPeriod: // .
+		if !strings.Contains(g.val, ".") {
+			g.val += "."
+		}
+
+	case ebiten.KeyE: // exponent
+
+	case ebiten.KeyEqual: // +
+		v, _ := strconv.ParseFloat(g.val, 64)
+		g.acc += v
+		g.val = ""
+
+	case ebiten.KeyMinus: // -
+		v, _ := strconv.ParseFloat(g.val, 64)
+		g.acc -= v
+		g.val = ""
+
+	case ebiten.KeyX: // *
+		v, _ := strconv.ParseFloat(g.val, 64)
+		g.acc *= v
+		g.val = ""
+
+	case ebiten.KeySlash: // /
+		v, _ := strconv.ParseFloat(g.val, 64)
+		g.acc /= v
+		g.val = ""
+
+	case ebiten.KeyC: // clear
+		if g.val == "" {
+			g.acc = 0
+		} else {
+			g.val = ""
+		}
+
+	case ebiten.KeyEnter: // =
+		g.acc, _ = strconv.ParseFloat(g.val, 64)
+		g.val = ""
+	}
+
+	log.Println("acc:", g.acc, "val:", g.val)
 }
 
 func main() {
